a98d9dac63cff0d329ab92740410f62638e22c5c
Ryan Kelly <rfkelly@mozilla.com>
2016-10-18 16:00:12 +1100

    chore(notifications): Remove the defunct fxa-notification-server

--- roles/notifications/files/generate_keys.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-openssl genrsa -out /data/fxa-notification-server/secret.pem 2048
-openssl rsa -in /data/fxa-notification-server/secret.pem -pubout -out /data/fxa-notification-server/public.pem
