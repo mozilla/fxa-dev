4b135706d8f86c0cc0a87c33ccdb654259df735d
John Morrison <jrgmorrison@gmail.com>
2017-03-18 19:39:00 -0700

    fix(utils): initial parallel docker pull

---
+++ aws/docker-pull.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Intended for use on the initial build of a box. Ansible will update/restart
+# if changed subseqsently.
+
+IMAGES=(
+  jrgm/123done
+  mozilla/fxa-auth-db-mysql
+  mozilla/fxa-auth-server
+  mozilla/fxa-basket-proxy
+  mozilla/fxa-content-server
+  mozilla/fxa-customs-server
+  mozilla/fxa-oauth-server
+  mozilla/fxa-profile-server
+  mysql
+)
+
+for f in ${IMAGES[*]}; do
+  docker pull $f & # run in background
+  sleep 0.1        # don't be greedy
+done
+
+FAIL=0
+for job in `jobs -p`; do
+  wait $job || let "FAIL+=1"
+done
+
+if [ "$FAIL" == "0" ]; then
+  echo Initial docker pull complete: $FAIL
+  exit $FAIL
+fi
+
+echo Initial docker pull failed: $FAIL
+exit $FAIL
